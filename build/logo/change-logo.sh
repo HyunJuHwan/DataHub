3b1e647d6e754f19b65194592f8d9752e76385f5
현주환 <hyunjw0306@neighbor21.co.kr>
2023-11-17 12:24:11 +0900

    security logo change

--- build/logo/change-logo.sh
+++ build/logo/change-logo.sh
@@ -1,13 +1,15 @@
 # core-dashboard 로고 변경
-cp ./logo/NDhub.png ./core/datacore/dashboard/frontend/public/img/city_data_hub_logo_W.png
+cp ./NDhub.png ../core/datacore/dashboard/frontend/public/img/city_data_hub_logo_W.png
 
 # core-ui 로고 변경
-cp ./logo/NDhub.png ./core/datacore/ui/frontend/src/assets/img/city_data_hub_logo_W.png
-cp ./logo/core_sub.css ./core/datacore/ui/frontend/src/assets/css/core_sub.css
+cp ./NDhub.png ../core/datacore/ui/frontend/src/assets/img/city_data_hub_logo_W.png
+cp ./core_sub.css ../core/datacore/ui/frontend/src/assets/css/core_sub.css
 
 #ingest 로고 변경
-cp ./logo/NDhub.png ./ingest/ingest/ingest-web/src/main/resources/static/asset/images/sprite/sp_com_n.png
-cp ./logo/ingest_sub.css ./ingest/ingest/ingest-web/src/main/resources/static/asset/css/ingest_sub.css
+cp ./NDhub.png ../ingest/ingest/ingest-web/src/main/resources/static/asset/images/sprite/sp_com_n.png
+cp ./ingest_sub.css ../ingest/ingest/ingest-web/src/main/resources/static/asset/css/ingest_sub.css
 
 #security 로고 변경
 
+cp ./NDhub2.png ../secure/security/smartcity_front/public/images/login/img_logo.png
+cp ./security_login.css ../secure/security/smartcity_front/src/stylesheets/login.css
