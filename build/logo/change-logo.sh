3ffe3489f4c9ae7a46e2af4c3a79ea3f52375a5d
현주환 <hyunjw0306@neighbor21.co.kr>
2023-11-17 16:13:54 +0900

    change-logo.sh 파일경로 수정

--- build/logo/change-logo.sh
+++ build/logo/change-logo.sh
@@ -3,13 +3,12 @@ cp ./NDhub.png ../core/datacore/dashboard/frontend/public/img/city_data_hub_logo
 
 # core-ui 로고 변경
 cp ./NDhub.png ../core/datacore/ui/frontend/src/assets/img/city_data_hub_logo_W.png
-cp ./core_sub.css ../core/datacore/ui/frontend/src/assets/css/core_sub.css
+cp ./core_sub.css ../core/datacore/ui/frontend/src/assets/css/sub.css
 
 #ingest 로고 변경
 cp ./NDhub.png ../ingest/ingest/ingest-web/src/main/resources/static/asset/images/sprite/sp_com_n.png
-cp ./ingest_sub.css ../ingest/ingest/ingest-web/src/main/resources/static/asset/css/ingest_sub.css
+cp ./ingest_sub.css ../ingest/ingest/ingest-web/src/main/resources/static/asset/css/sub.css
 
 #security 로고 변경
-
 cp ./NDhub2.png ../secure/security/smartcity_front/public/images/login/img_logo.png
 cp ./security_login.css ../secure/security/smartcity_front/src/stylesheets/login.css
