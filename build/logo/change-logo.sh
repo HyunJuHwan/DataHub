8166a180f14b6b22a8ed1c20409307bb067db841
현주환 <hyunjw0306@neighbor21.co.kr>
2023-11-17 12:00:06 +0900

    logo change, dashboard subscription update

---
+++ build/logo/change-logo.sh
@@ -0,0 +1,13 @@
+# core-dashboard 로고 변경
+cp ./logo/NDhub.png ./core/datacore/dashboard/frontend/public/img/city_data_hub_logo_W.png
+
+# core-ui 로고 변경
+cp ./logo/NDhub.png ./core/datacore/ui/frontend/src/assets/img/city_data_hub_logo_W.png
+cp ./logo/core_sub.css ./core/datacore/ui/frontend/src/assets/css/core_sub.css
+
+#ingest 로고 변경
+cp ./logo/NDhub.png ./ingest/ingest/ingest-web/src/main/resources/static/asset/images/sprite/sp_com_n.png
+cp ./logo/ingest_sub.css ./ingest/ingest/ingest-web/src/main/resources/static/asset/css/ingest_sub.css
+
+#security 로고 변경
+
